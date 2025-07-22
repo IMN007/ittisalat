b4f8241ac1a72b8012e1f9df9210b3edec6cbdda
Idris Mohamednur <idris_m_nur@hotmail.com>
2025-07-22 15:04:31 +0200

    Moved pluginManagement to top of project-level build.gradle.kts (final fix)

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -3,6 +3,7 @@
 // Den är skriven i Kotlin DSL.
 
 // Detta block hanterar konfigurationen av plugins som används i hela projektet.
+// DETTA MÅSTE VARA DET FÖRSTA BLOCKET I FILEN.
 pluginManagement {
     repositories {
         // Gradle Plugin Portal är standard för de flesta Gradle-plugins.
@@ -25,19 +26,19 @@ buildscript {
     }
     // Definierar beroenden som behövs för att bygga projektet självt (t.ex. Gradle-plugins).
     dependencies {
-        // Firebase Google Services plugin. Denna är nödvändig för att din app ska kunna använda Firebase.
-        // VIKTIGT: Använd den version som Firebase Console rekommenderar (t.ex. 4.4.2).
+        // Firebase Google Services plugin. Denna är nödvändig för att din app ska kunna anvnda Firebase.
+        // VIKTIGT: Anvnd den version som Firebase Console rekommenderar (t.ex. 4.4.2).
         classpath("com.google.gms:google-services:4.4.2")
 
-        // Dessa är standardplugins för Android Gradle Plugin och Kotlin Gradle Plugin.
-        // De bör redan finnas i din fil, men se till att versionerna matchar ditt projekt.
+        // Dessa r standardplugins fr Android Gradle Plugin och Kotlin Gradle Plugin.
+        // De br redan finnas i din fil, men se till att versionerna matchar ditt projekt.
         // Om de saknas, uncommenta dem och justera versionerna vid behov.
         // classpath("com.android.tools.build:gradle:8.1.2") // Exempel, anpassa till din version
         // classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.0") // Exempel, anpassa till din version
     }
 }
 
-// Konfigurerar repositories för alla underprojekt i ditt Android-projekt.
+// Konfigurerar repositories fr alla underprojekt i ditt Android-projekt.
 allprojects {
     repositories {
         google()
@@ -45,22 +46,22 @@ allprojects {
     }
 }
 
-// Definierar en ny byggkatalog för att undvika konflikter med Flutter's byggkatalog.
-// Denna del av koden är specifik för Flutter-projekt.
+// Definierar en ny byggkatalog fr att undvika konflikter med Flutter's byggkatalog.
+// Denna del av koden r specifik fr Flutter-projekt.
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
-// Konfigurerar underprojekt för att säkerställa korrekt byggordning.
+// Konfigurerar underprojekt fr att skerstlla korrekt byggordning.
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
 subprojects {
-    // Säkerställer att ':app' modulen utvärderas innan andra underprojekt.
+    // Skerstller att ':app' modulen utvrderas innan andra underprojekt.
     project.evaluationDependsOn(":app")
 }
 
-// Definierar en 'clean'-uppgift för att rensa byggkatalogerna.
+// Definierar en 'clean'-uppgift fr att rensa byggkatalogerna.
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
